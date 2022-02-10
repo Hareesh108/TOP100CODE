38af3b8a92f99c53eb6c73c33319a9f7f0a1a753
HAREESH BHITTAM <72018706+Hareesh108@users.noreply.github.com>
2022-02-10 22:23:42 +0530

    Add files via upload

---
+++ 16_Fibonachi_Nth_Number.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int F(int N)
+{
+    int a = 0, b = 1, c, i;
+    if( N == 0)
+        return a;
+    for(i = 2; i <= N; i++)
+    {
+       c = a + b;
+       a = b;
+       b = c;
+    }
+    return b;
+}
+ 
+// Driver code
+int main()
+{
+    int N;
+    cout << "Enter the nth position";
+    cin >> N;
+     
+    cout << F(N);
+    return 0;
+}
